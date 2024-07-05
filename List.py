8b2eca6f44fe7f7caa5565d599bb3b19bd73bd41
imabuzerkhan <abuzerk41@gmail.com>
2024-07-05 15:36:43 +0545

    list

---
+++ List.py
@@ -0,0 +1,13 @@
+my_list = [1, 2, 3, 'hello', True]
+print(my_list)
+
+Workers_list = [10,'ramesh' , 'suresh' , 'satish', 'shankar' , 'aaiyar' , True ]
+# Workers_list.append("abuzer")
+# Workers_list.insert(3,'bakar bhai')
+# Workers_list[2] = "danish"
+# Workers_list.remove(1)
+# Workers_list.pop()
+Workers_list[1:4]
+print(Workers_list)
+# print(len(Workers_list))
+# print(Workers_list[4])
