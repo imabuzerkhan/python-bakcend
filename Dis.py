d89bbedbad75734b1a01af2e29439e9112dd6e5b
imabuzerkhan <abuzerk41@gmail.com>
2024-07-05 15:47:40 +0545

    dis push

---
+++ Dis.py
@@ -0,0 +1,12 @@
+Student_record = {
+  'name' : 'abuzer khan' ,
+  'age' : 22 ,
+  'college' : 'nepal business college',
+  'course': 'BIT'
+}
+Student_record['city'] = 'Nepal'
+Student_record['food'] = ['chicken' , 'mutton' 'vegetable' ] 
+del Student_record['age']
+print(Student_record['name']);
+print(Student_record['course'])
+print(Student_record)
