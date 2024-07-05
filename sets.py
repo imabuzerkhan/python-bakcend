5108dbdfff831a87c87b16514fb05c01beaf14fa
imabuzerkhan <abuzerk41@gmail.com>
2024-07-05 16:08:27 +0545

    sets

---
+++ sets.py
@@ -0,0 +1,12 @@
+collection = {'hello' , 'hello' , '1' , '2' , '1' , 'mannu' }
+collection.add(3)
+collection.remove('mannu')
+collection.pop()
+collection.clear()
+print(collection)
+print(len(collection))
+
+
+# * empty set
+new = set()
+print(type(new))
